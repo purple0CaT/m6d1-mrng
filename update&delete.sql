c4aa10b1cd43d4a9b6f52c34a2ad1e5ff75df30e
Istvan <17463906+purple0CaT@users.noreply.github.com>
2021-09-27 17:45:24 +0300

    fix id

--- update&delete.sql
+++ update&delete.sql
@@ -1,5 +1,5 @@
 UPDATE books SET cover='New Cover' WHERE book_id='2' RETURNING *;
-UPDATE authors SET birth_year='2021' WHERE author_id='1' RETURNING *;
+UPDATE authors SET birth_year='1985' WHERE author_id='2' RETURNING *;
 -- DELETE
 DELETE FROM authors WHERE country='U.S.' RETURNING *
 DELETE FROM books WHERE category='novel' RETURNING *
